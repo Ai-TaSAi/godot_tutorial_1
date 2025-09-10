24e0d1658da9eaeeff31c8db8a854476be9a8f93
Rui Wen <ruiwen2002@gmail.com>
2025-09-09 19:53:43 -0600

    Update 4: (In case WASD is not good enough) - Added ship rotation and maneuvering, and updated background music. Finalized last of the sprites.

--- creepstutorial/player.gd
+++ creepstutorial/player.gd
@@ -4,39 +4,67 @@ signal hit
 @export var speed = 400 # How fast the player moves, px/s
 var screen_size # Size of game window
 
+# New variables for the ship to use for rotational and speed. Speed can now be independently adjusted.
+var facing_degrees = 0
+var ship_speed = 0.8
+
 func _ready(): # Used when the player enters the scene tree.
 	screen_size = get_viewport_rect().size
+	facing_degrees = 270
 	hide() # Hides the player upon the start of the game.
 	
 func _process(delta): # Runs the whole time.
 	var velocity = Vector2.ZERO # Defines player's movement vector, add and subtract to modify the velocity based on player movement direction.
-	if Input.is_action_pressed("move_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("move_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("move_down"):
-		velocity.y += 1
-	if Input.is_action_pressed("move_up"):
-		velocity.y -= 1
+	
+	# OLD TUTORIAL CODE. NOT USED.
+	#if Input.is_action_pressed("move_right"):
+		#velocity.x += 1
+	#if Input.is_action_pressed("move_left"):
+		#velocity.x -= 1
+	#if Input.is_action_pressed("move_down"):
+		#velocity.y += 1
+	#if Input.is_action_pressed("move_up"):
+		#velocity.y -= 1
 		
+	# Left and Right now rotate the ship instead of making it go up and down.
+	if Input.is_action_pressed("move_right"):
+		facing_degrees += 5
+		if (facing_degrees > 360):
+			facing_degrees = 0
+	if Input.is_action_pressed("move_left"):
+		facing_degrees -= 5
+		if (facing_degrees < 0):
+			facing_degrees = 360
+	# Fly ship forward or reverse it (though realistically, you're not able to reverse a ship in such manners)
+	if Input.is_action_pressed("move_up"):
+		velocity.x += cos(facing_degrees * PI / 180) * ship_speed
+		velocity.y += sin(facing_degrees * PI / 180) * ship_speed
+	if Input.is_action_pressed("move_down"):
+		velocity.x -= cos(facing_degrees * PI / 180) * ship_speed
+		velocity.y -= sin(facing_degrees * PI / 180) * ship_speed
+		
+	$AnimatedSprite2D.rotation = (facing_degrees * PI / 180)
+		
+	# Update code to only use fly animation, when ship is in motion.
 	if velocity.length() > 0: # Moving @ (1,1) = SQRT(2) is faster than simply moving cardinal. Normalizing prevents that.
 		velocity = velocity.normalized() * speed
 		# Play sprite if moving. Else don't.
 		$AnimatedSprite2D.play()
+		$AnimatedSprite2D.animation = "fly"
 	else:
 		$AnimatedSprite2D.stop()
 		
 	position += velocity * delta # Modify character position based on the velocity, by the system FPS.
 	position = position.clamp(Vector2.ZERO, screen_size) # Clamp prevents the character from leaving the screen.
 	
-	# Choosing animation files to play based on direction of sprite.
-	if velocity.x != 0:
-		$AnimatedSprite2D.animation = "walk"
-		$AnimatedSprite2D.flip_v = false
-		$AnimatedSprite2D.flip_h = velocity.x < 0
-	elif velocity.y != 0:
-		$AnimatedSprite2D.animation = "up"
-		$AnimatedSprite2D.flip_v = velocity.y > 0
+	# Choosing animation files to play based on direction of sprite - OLD CODE, NOT USED ANYMORE.
+	#if velocity.x != 0:
+		#$AnimatedSprite2D.animation = "walk"
+		#$AnimatedSprite2D.flip_v = false
+		#$AnimatedSprite2D.flip_h = velocity.x < 0
+	#elif velocity.y != 0:
+		#$AnimatedSprite2D.animation = "up"
+		#$AnimatedSprite2D.flip_v = velocity.y > 0
 
 func _on_body_entered(body: Node2D) -> void:
 	hide() # Player disappears after being hit.
